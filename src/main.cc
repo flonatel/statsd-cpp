4a6b308bd0e8905d92f6794306933195a23ceebb
Andreas Florath <info@flonatel.org>
2014-03-23 08:40:47 +0100

    Switched on tracing because looks there is some problem with the reading

--- src/main.cc
+++ src/main.cc
@@ -7,6 +7,20 @@ namespace statsd {
 
 using namespace pegtl;
 
+struct statsd_data {
+   std::string name;
+   uint64_t    counter_increment;
+   char        type;
+};
+
+struct set_name
+   : action_base< set_name >  {
+   static void apply(std::string const & m, statsd_data & s) {
+      s.name = m;
+      std::cout << "Set name [" << m << "]" << std::endl;
+   }
+};
+
 // This is simple, because currently only counters 'c' are supported.
 struct read_type_char
    : string<'c'> {};
@@ -14,24 +28,25 @@ struct read_type_char
 struct read_counter_increment
    : plus< digit > {};
 
-struct read_counter_name
+struct read_name
    : plus< sor< alnum, string<'.'> > > {};
 
 struct read_statsd_line
-   : seq<read_counter_name,
-         string<':'>,
-         read_counter_increment,
-         string<'|'>,
-         read_type_char,
-         eol> {};
+   : seq< ifapply< read_name, set_name >,
+          string<':'>,
+          read_counter_increment,
+          string<'|'>,
+          read_type_char,
+          eol> {};
 
 struct read_statsd_protocol
    : seq<read_statsd_line, eof> {};
 }
 
 int main() {
-   pegtl::basic_parse_input< statsd::read_statsd_protocol >(
+   statsd::statsd_data data;
+   pegtl::trace_parse_input< statsd::read_statsd_protocol >(true,
       std::istream_iterator< char >( std::cin ),
-      std::istream_iterator< char >() );
+      std::istream_iterator< char >(), data);
    return 0;
 }
