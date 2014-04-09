32a27ee5ad019d8582cb8201f7857f4759fb16f6
Andreas Florath <info@flonatel.org>
2014-04-09 09:05:06 +0200

    Moved common things to test class - added second test case

--- lib/statsdcpp/unittests/CounterSimpleTest.cc
+++ lib/statsdcpp/unittests/CounterSimpleTest.cc
@@ -5,15 +5,21 @@
 #include <statsdcpp/output/string.hh>
 
 class CounterSimple : public ::testing::Test {
+public:
+   CounterSimple()
+      : debug_serializer(string_writer),
+        collector(debug_serializer) {
+   }
+
+   statsdcpp::output::string string_writer;
+   statsdcpp::serializer::debug<statsdcpp::output::string>
+   debug_serializer;
+   statsdcpp::collector< statsdcpp::serializer::debug<
+                            statsdcpp::output::string> >
+      collector;
 };
 
 TEST_F(CounterSimple, test_one_counter) {
-   statsdcpp::output::string string_writer;
-   statsdcpp::serializer::debug<statsdcpp::output::string>
-      debug_serializer(string_writer);
-   statsdcpp::collector< statsdcpp::serializer::debug<
-      statsdcpp::output::string> >
-      collector(debug_serializer);
    statsdcpp::counter_sp cnt(
       collector.generate_counter("org.flonatel.statsd.test.cnt1"));
    cnt->inc();
@@ -21,6 +27,21 @@ TEST_F(CounterSimple, test_one_counter) {
    collector.flush();
 
    ASSERT_EQ("Counter:org.flonatel.statsd.test.cnt1:1\n",
+             string_writer.str());
+}
+
+TEST_F(CounterSimple, test_two_counter) {
+   statsdcpp::counter_sp cnt1(
+      collector.generate_counter("org.flonatel.statsd.test.cnt1"));
+   cnt1->inc();
+   statsdcpp::counter_sp cnt2(
+      collector.generate_counter("org.flonatel.statsd.test.cnt2"));
+   cnt2->inc();
+
+   collector.flush();
+
+   ASSERT_EQ("Counter:org.flonatel.statsd.test.cnt1:1\n"
+             "Counter:org.flonatel.statsd.test.cnt2:1\n",
              string_writer.str());
 }
 
